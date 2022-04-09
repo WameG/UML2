7ac20a3cbe71dc16eb7ffa9692db81fcdefa0ca1
wamegassama <wame0003@edu.zealand.dk>
2022-04-09 12:24:46 +0200

    * .gitignore: First commit
    * PizzaStore2.sln:
    * Pizza.cs:
    * Store.cs:
    * Program.cs:
    * FastFood.cs:
    * MenuChoice.cs:
    * MenuCatalog.cs:
    * PizzaStore2.csproj:

---
+++ PizzaStore2/FastFood.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaStore2
+{
+    public class FastFood
+    {
+        List<string> FastFoodMenu;
+
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+
+        public FastFood()
+        {
+            Id = 0;
+            Name = "";
+            Price = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: {Price}";
+        }
+    }
+}
---
+++ PizzaStore2/Store.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaStore2
+{
+    public class Store
+    {
+        public void Start()
+        {
+            Test();
+        }
+
+        public void Test()
+        {
+            //MenuCatalog menuCatalog = new MenuCatalog();
+
+            //Pizza p1 = new Pizza(63, "Margherita", 72);
+            //Pizza p2 = new Pizza(34, "Vesuvio", 78);
+            //Pizza p3 = new Pizza(78, "Calzone", 83);
+            //Pizza p4 = new Pizza(54, "Tonno", 88);
+
+            ////Adding Pizzas To Menu
+
+            //menuCatalog.CreatePizza(p1);
+            //menuCatalog.CreatePizza(p2);
+            //menuCatalog.CreatePizza(p3);
+
+            ////Reading Pizzas in menu
+
+            //Console.WriteLine("-------------------------------------");
+            //Console.WriteLine("| Pizza Menu Before Crud Operations |");
+            //Console.WriteLine("-------------------------------------\n");
+
+            //Console.WriteLine(menuCatalog.ReadPizza(63));
+            //Console.WriteLine(menuCatalog.ReadPizza(34));
+            //Console.WriteLine(menuCatalog.ReadPizza(78));
+
+            ////Updating Pizzas in menu
+
+            //menuCatalog.UpdatePizza(63, p4);
+
+            ////Deleting pizzas in menu
+
+            //menuCatalog.DeletePizza(p2);
+
+
+            //Console.WriteLine("\n------------------------------------");
+            //Console.WriteLine("| Pizza Menu After Crud Operations |");
+            //Console.WriteLine("------------------------------------\n");
+
+            //menuCatalog.PrintMenu();
+
+            //Console.WriteLine(menuCatalog.SearchPizza("Tonno"));
+
+            UserDialog.Run();
+        }
+    }
+}
